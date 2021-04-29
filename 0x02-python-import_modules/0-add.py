3b7b0598499108d3dfbdd6367b2fb72e6fa70897
Jose Vazquez <vazqjose@gmail.com>
2021-04-29 20:59:29 +0000

    test

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def add(a, b):
+    if __name__ == "__main__": 
+        a = 1
+        b = 2
+        print("{} + {} = {}".format(a, b, a + b))
+    else:
+        return(a + b)
