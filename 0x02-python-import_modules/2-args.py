34bdee5150232b3281da0bd5c90ffe3f37fa970e
Jose Vazquez <vazqjose@gmail.com>
2021-04-29 22:41:35 +0000

    t2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+import sys
+argv = sys.argv
+argLen = len(argv)
+string = "argument"
+
+if argLen == 1:
+    string = string + "s."
+elif argLen == 2:
+    string = string + ":"
+elif argLen > 2:
+    string = string + "s:"
+
+print("{} {}".format(argLen - 1, string))
+
+for n in range(1, argLen):
+    print("{}: {}".format(n, argv[n]))
