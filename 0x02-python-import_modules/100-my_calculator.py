c32be0dc6255713aca2bdfbb1dd8f6aa1d86717f
Jose Vazquez <vazqjose@gmail.com>
2021-04-30 00:27:36 +0000

    adv1

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+
+    import sys
+    from calculator_1 import add, sub, mul, div
+
+    myArgs = sys.argv
+
+    if len(myArgs) != 4:
+        print('Usage: ./100-my_calculator.py <a> <operator> <b>')
+        exit(1)
+    else:
+        a = int(myArgs[1])
+        b = int(myArgs[3])
+        operator = myArgs[2]
+
+    if operator not in ['+', '-', '*', '/']:
+        print('Unknown operator. Available operators: +, -, * and /')
+        exit(1)
+
+    if operator == '+':
+        print(add(a, b))
+
+    if operator == '-':
+        print(sub(a, b))
+
+    if operator == '*':
+        print(mul(a, b))
+
+    if operator == '/':
+        print(div(a, b))
