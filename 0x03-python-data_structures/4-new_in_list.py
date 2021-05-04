3a74a78fe2e1136e1146d29c331a8b5b4d036f48
Jose Vazquez <vazqjose@gmail.com>
2021-05-04 20:58:50 +0000

    t4

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+
+    if idx < 0:
+        return(my_list)
+    elif idx > len(my_list) - 1:
+        return(my_list)
+
+    listcpy = my_list
+    listcpy[idx] = element
+    return (listcpy)
