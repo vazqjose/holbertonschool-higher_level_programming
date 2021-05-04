cd4f9e2c52e7e69286e2ef91567a1484516089a2
Jose Vazquez <vazqjose@gmail.com>
2021-05-04 21:23:17 +0000

    t4

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
 
+    if my_list is None:
+        exit
+
     if idx < 0:
         return(my_list)
     elif idx > len(my_list) - 1:
         return(my_list)
-
-    if my_list is None:
-        exit
     else:
-        listcpy = my_list
+        listcpy = my_list[:]
         listcpy[idx] = element
         return (listcpy)
