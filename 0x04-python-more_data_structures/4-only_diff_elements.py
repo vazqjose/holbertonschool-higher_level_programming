6f08c131a63387ad37873a789ffabfa34f683851
Jose Vazquez <vazqjose@gmail.com>
2021-05-06 23:35:18 +0000

    t4

---
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,15 @@
+#!/usr/bin/pythom3
+def only_diff_elements(set_1, set_2):
+
+    newList = []
+
+    for item in set_1:
+        if item not in set_2:
+            newList.append(item)
+
+    for item in set_2:
+        if item not in set_1:
+            newList.append(item)
+
+    return newList
+
