804d62db7fda0dd8fa175fc19cca366f5966ba92
Jose Vazquez <vazqjose@gmail.com>
2021-05-07 00:16:11 +0000

    t6

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def print_sorted_dictionary(a_dictionary):
+
+    dictionary_items = a_dictionary.items()
+    sorted_items = sorted(dictionary_items)
+
+    for key, item in sorted_items:
+        print('{}: {}'.format(key, item))
+
+    #print(sorted_items)
