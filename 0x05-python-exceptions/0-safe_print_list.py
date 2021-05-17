b7a3b9aba82639570513b91b219843fca91f9f1e
Jose Vazquez <vazqjose@gmail.com>
2021-05-17 23:17:21 +0000

    printing format

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,7 +2,6 @@
 
 def safe_print_list(my_list=[], x=0):
 
-    list2 = []
     i = 0
 
     try:
@@ -14,4 +13,5 @@ def safe_print_list(my_list=[], x=0):
         return i
 
     except IndexError:
+        print()
         return i
