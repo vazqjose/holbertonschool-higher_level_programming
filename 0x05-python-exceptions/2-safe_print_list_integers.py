bd5580ebdd6c0e7bff22f5199cef91012d687b10
Jose Vazquez <vazqjose@gmail.com>
2021-05-18 01:45:44 +0000

    t2

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -13,6 +13,8 @@ def safe_print_list_integers(my_list=[], x=0):
             continue
         except ValueError:
             continue
+        except NameError:
+            continue
 
     print()
     return i
