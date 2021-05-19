9471887fc6cf31502dfb4c1a24ad0cd52fab75ce
Jose Vazquez <vazqjose@gmail.com>
2021-05-19 00:17:45 +0000

    t2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+""" lets add an attribute to our class """
+
+
+class Square:
+    """ create the size attribute """
+
+    def __init__(self, size=0):
+        self.__size = size
+
+    def validate(self):
+
+        """ validate the size variablo received from calling program """
+
+
+        if not size.isdigit():
+            raise TypeError("size must be an integer")
+
+        if self.__size < 0:
+            raise ValueError("size must be >= 0")
