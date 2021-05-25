aa42dcbb3624f3d9d779ef760827f08cdb81043f
Jose Vazquez <vazqjose@gmail.com>
2021-05-25 21:51:06 +0000

    t9

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -42,7 +42,7 @@ class Rectangle:
     def width(self, value):
 
         if type(value) is not int:
-            raise TypeError("width is not an integer")
+            raise TypeError("width must be an integer")
         elif value < 0:
             raise ValueError("width must be >= 0")
         else:
@@ -56,7 +56,7 @@ class Rectangle:
     def height(self, value):
 
         if type(value) is not int:
-            raise TypeError("height is not an integer")
+            raise TypeError("height must be an integer")
         elif value < 0:
             raise ValueError("height must be >= 0")
         else:
@@ -93,5 +93,5 @@ class Rectangle:
             return rect_2
 
     @classmethod
-    def square(cls, size):
+    def square(cls, size=0):
         return cls(size, size)
