70fb6cd4713ca56d091279a5b1d59277a1ec14e7
Jose Vazquez <vazqjose@gmail.com>
2021-05-27 04:23:54 +0000

    t5

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,5 +1,16 @@
 #!/usr/bin/python3
+"""
+Write a function that prints a text with 2 new lines after . ? and :
+Args:
+    text: input string
+Returns:
+    Nothing
+"""
+
+
 def text_indentation(text):
+    """ returns string with 2 line breaks """
+
     if type(text) is str:
         for c in text:
             if c == '.' or c == '?' or c == ':':
