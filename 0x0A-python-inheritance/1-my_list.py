d360445fa2b10fb1130a6affaab048b278574637
Jose Vazquez <vazqjose@gmail.com>
2021-06-02 22:08:26 +0000

    t1

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+class MyList(list):
+"""
+    class MyList that inherits from list
+"""
+
+
+    def print_sorted(self):
+        """ return sorted list """
+
+        return sorted(self)
