b132a38564129942f0daec9c547fe8e036b07475
Jose Vazquez <vazqjose@gmail.com>
2021-06-03 02:53:26 +0000

    t6

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -4,6 +4,7 @@ Write a class BaseGeometry (based on 5-base_geometry.py)
 """
 
 class BaseGeometry:
+    """ class name based on previous task """
 
     def area(self):
         """ Raises an Exception """
