db4df7268642103bbd3576dea15d55dc39e68010
Jose Vazquez <vazqjose@gmail.com>
2021-06-03 02:00:08 +0000

    t3

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+a function that returns True if the object is an instance
+of, or if the object is an instance of a class that inherited from,
+the specified class ; otherwise False.
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    args:
+        obj: object to check
+        a_class: class to verify against
+    """
+
+    return(isinstance(obj, a_class))
