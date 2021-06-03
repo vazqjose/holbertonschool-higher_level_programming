8fed64d2642e3fee06b25137ab5359b09f0f51d0
Jose Vazquez <vazqjose@gmail.com>
2021-06-03 03:55:09 +0000

    t8

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
 """
 Write a class Rectangle that inherits from BaseGeometry (7-base_geometry.py)
 """
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
@@ -12,7 +14,7 @@ class Rectangle(BaseGeometry):
         """ initiate objects and call parent method """
 
         self.__width = width
-        self.__height = height
-
         super().integer_validator("width", width)
+
+        self.__height = height
         super().integer_validator("height", height)
