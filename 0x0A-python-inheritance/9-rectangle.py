d7a800a69c5f498246e81b4e74dea1f881a2e2d4
Jose Vazquez <vazqjose@gmail.com>
2021-06-03 03:58:18 +0000

    t9

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Write a class Rectangle that inherits from BaseGeometry (7-base_geometry.py)
+"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """ create a rectangle class """
+
+    def __init__(self, width, height):
+        """ initiate objects and call parent method """
+
+        self.__width = width
+        super().integer_validator("width", width)
+
+        self.__height = height
+        super().integer_validator("height", height)
+
+    def area():
+
+    def print():
+
