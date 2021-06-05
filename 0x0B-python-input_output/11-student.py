e7df6b3e9372876ae9c7e3e23e5ca86e5e9cfb41
Jose Vazquez <vazqjose@gmail.com>
2021-06-05 05:04:24 +0000

    t11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Write a class Student that defines a student by:
+
+Public instance attributes: first_name, last_name, age
+"""
+
+
+class Student:
+    """ Instantiation with first_name, last_name and age """
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        myDict = {}
+
+        if attrs is None:
+            return(self.__dict__)
+        else:
+            for n in attrs:
+                #  if n exists in self.__dict__[key][value]
+                if n in self.__dict__.keys():
+                    myDict[n] = self.__dict__[n]
+            return myDict
+
+    def reload_from_json(self, json):
+        for key, value in json.items(): # ("Fake", "Fake", 89)
+            setattr(self, key, value)
