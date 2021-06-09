bb746f0edfd2c3ff6d7508e2160405a930f5c529
Jose Vazquez <vazqjose@gmail.com>
2021-06-09 03:30:05 +0000

    16

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -38,6 +38,7 @@ class Base:
         else:
             return (json.dumps(list_dictionaries))
 
+    @classmethod
     def save_to_file(cls, list_objs):
         myList = []
         filename = cls.__name__ + ".json"
