c69e7602590bbb5d8dbf7336cdec39a0ac2b9b62
Jose Vazquez <vazqjose@gmail.com>
2021-06-09 03:59:44 +0000

    t16

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -4,15 +4,6 @@
 """
 Class Base:
 private class attribute __nb_objects = 0
-class constructor: def __init__(self, id=None):
-    * if id is not None, assign the public instance attribute id with
-    this argument value - you can assume id is an integer and you don’t
-    need to test the type of it otherwise, increment __nb_objects and 
-    assign the new value to the public instance attribute id
-
-    * This class will be the “base” of all other classes in this project.
-    The goal of it is to manage id attribute in all your future classes and to 
-    avoid duplicating the same code (by extension, same bugs)
 """
 
 
@@ -26,7 +17,7 @@ class Base:
 
     def __init__(self, id=None):
 
-        if id != None:
+        if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
@@ -34,10 +25,10 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
-        if list_dictionaries is None or len(list_dictionaries) == 0:
-            return "[]"
-        else:
+        if len(list_dictionaries) > 0:
             return (json.dumps(list_dictionaries))
+        else:
+            return "[]"
 
     @classmethod
     def save_to_file(cls, list_objs):
@@ -56,4 +47,3 @@ class Base:
             return "[]"
         else:
             return json.loads(json_string)
-
