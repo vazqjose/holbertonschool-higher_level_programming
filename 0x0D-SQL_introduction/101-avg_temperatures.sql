9a161e61ca0afb4d74d8a90d11a44ced797d238f
Jose Vazquez <vazqjose@gmail.com>
2021-07-14 02:02:57 +0000

    t101

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,4 +1,11 @@
 -- import a sql file into database
-USE hbtn_0c_0;
-mysql -u root --password=456011 hbtn_0c_0 < temperatures.sql;
+SELECT 
+	city, 
+	AVG(value) AS avg_temp
+FROM 
+	temperatures
+GROUP BY 
+	city
+ORDER BY avg_temp DESC;
+
 
