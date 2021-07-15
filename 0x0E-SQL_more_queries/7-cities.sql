ae6839d6530a55dcb35cde09c20ad5413a8d3744
Jose Vazquez <vazqjose@gmail.com>
2021-07-15 02:08:16 +0000

    t7

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -12,5 +12,5 @@ CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
 	id INT NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY,
 	name VARCHAR(256) NOT NULL,
 	state_id INT NOT NULL,
-	FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states(id)
+	FOREIGN KEY (state_id) REFERENCES hbtn_0d_usa.states(id)
 );
