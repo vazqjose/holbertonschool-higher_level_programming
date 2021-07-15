9481a07e001c9777c11178aedfb2c88791cb6bc8
Jose Vazquez <vazqjose@gmail.com>
2021-07-15 00:42:40 +0000

    t1

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
 -- create the MySQL server user user_0d_1
 DROP USR IF EXISTS 'user_0d_1'@'localhost';
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-
+GRANT ALL ON * . * TO 'user_0d_1'@'localhost';
