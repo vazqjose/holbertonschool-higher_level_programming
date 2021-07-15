468764b89984ead0c4dea99af5fe982db5d198a6
Jose Vazquez <vazqjose@gmail.com>
2021-07-15 01:31:57 +0000

    t5

---
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -0,0 +1,9 @@
+-- Create the table unique_id:
+-- 		id INT with the default value 1 and must be unique
+-- 		name VARCHAR(256)
+-- If the table unique_id already exists, your script should not fail
+CREATE TABLE IF NOT EXISTS unique_id (
+	id INT DEFAULT '1' UNIQUE,
+	name VARCHAR(256)
+);
+
