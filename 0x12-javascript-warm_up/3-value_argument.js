40cfb8a9c32533eafe0bd91d012185b213b90479
Jose Vazquez <vazqjose@gmail.com>
2021-08-03 02:47:22 +0000

    t3

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const process = require('process');
+
+const argv = process.argv;
+
+if (argv[2] === undefined) {
+  console.log('No argument');
+} else {
+  console.log(argv[2]);
+}
