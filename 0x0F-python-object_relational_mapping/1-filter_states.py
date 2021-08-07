706253370a95f82b41932eb4e1bffd02131d6bcd
Jose Vazquez <vazqjose@gmail.com>
2021-08-07 00:58:38 +0000

    pep8 fix

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 """
-Write a script that lists all states with a name starting with N (upper N) 
+Write a script that lists all states with a name starting with N (upper N)
 from the database hbtn_0e_0_usa:
 
-    - Your script should take 3 arguments: mysql username, mysql password and
+    - Your script should take 3 arguments: mysql username,
+        mysql password and
         database name (no argument validation needed)
     - You must use the module MySQLdb (import MySQLdb)
-    - Your script should connect to a MySQL server running on localhost at port 3306
+    - Your script should connect to a MySQL server running
+        on localhost at port 3306
     - Results must be sorted in ascending order by states.id
     - Results must be displayed as they are in the example below
     - Your code should not be executed when imported
@@ -16,13 +18,17 @@ from the database hbtn_0e_0_usa:
 import sys
 import MySQLdb
 
-db = MySQLdb.connect(host="localhost", user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                     passwd=sys.argv[2], db=sys.argv[3])
 
 result = db.cursor()
 
-result.execute(""" SELECT states.id, states.name FROM states WHERE states.name LIKE 'N%' ORDER BY states.id ASC """)
+result.execute("SELECT states.id, states.name FROM states
+               WHERE states.name LIKE 'N%' ORDER BY states.id ASC ")
 
 myRow = result.fetchall()
 
 for row in myRow:
     print(row)
+
+db.close()
