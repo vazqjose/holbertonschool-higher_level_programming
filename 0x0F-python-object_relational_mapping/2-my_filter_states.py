12d9f9844623716056a971a0e79e7f9981d087cb
Jose Vazquez <vazqjose@gmail.com>
2021-08-08 16:25:02 +0000

    t2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -3,15 +3,7 @@
 Write a script that takes in an argument and displays all values
 in the states table of hbtn_0e_0_usa where name matches the argument.
 
-    - Your script should take 4 arguments: mysql username, mysql
-        password, database name and state name searched (no argument
-        validation needed)
-    - You must use the module MySQLdb (import MySQLdb)
-    - Your script should connect to a MySQL server running on localhost
-        at port 3306
-    - You must use format to create the SQL query with the user input
     - Results must be sorted in ascending order by states.id
-    - Results must be displayed as they are in the example below
 """
 
 
@@ -24,8 +16,9 @@ if __name__ == "__main__":
     passwd = sys.argv[2]
     dbname = sys.argv[3]
     state = sys.argv[4]
-    sql = "SELECT states.id, states.name FROM states WHERE \
-            states.name='state' ORDER BY states.id ASC;"
+
+    sql = "SELECT * FROM states WHERE name LIKE BINARY '{}'\
+            ORDER BY id ASC;".format(state)
 
     dbConn = MySQLdb.connect(host, user, passwd, dbname)
     result = dbConn.cursor()
