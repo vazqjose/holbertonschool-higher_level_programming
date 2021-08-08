5a5c8694a3bf97eaa84a5b2cc210e801ec40c112
Jose Vazquez <vazqjose@gmail.com>
2021-08-08 16:57:10 +0000

    t3

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -7,36 +7,32 @@ the argument. But this time, write one that is safe from MySQL injections!
     - Your script should take 4 arguments: mysql username, mysql password,
         database name and state name searched (safe from MySQL injection)
     - You must use the module MySQLdb (import MySQLdb)
-    - Your script should connect to a MySQL server running on localhost
-        at port 3306
     - Results must be sorted in ascending order by states.id
-    - Results must be displayed as they are in the example below
 """
 
 import sys
 import MySQLdb
 
-dbConn = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3])
+if __name__ == "__main__":
 
-with dbConn.cursor() as result:
-    result.execute("""
-        SELECT
-            states.id, states.name
-        FROM
-            states
-        WHERE
-            states.name = %(name)s
-        ORDER BY
-            states.id
-        ASC
-        """, {
-            'name': sys.argv[4]
-        })
+    host = 'localhost'
+    user = sys.argv[1]
+    passwd = sys.argv[2]
+    db = sys.argv[3]
+    state = sys.argv[4].split(' ')
+    state = state[0]
+    sql = "SELECT * FROM states WHERE name LIKE BINARY '{}'\
+            ORDER BY id ASC".format(state)
 
-    myRow = result.fetchall()
+    dbConn = MySQLdb.connect(host, user, passwd, db)
+    result = dbConn.cursor()
 
-    for row in myRow:
-        print(row)
+    try:
+        result.execute(sql)
+        myRows = result.fetchall()
+        for row in myRows:
+            print(row)
+    except:
+        print("Could not display data")
 
     dbConn.close()
