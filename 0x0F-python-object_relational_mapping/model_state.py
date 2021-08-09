fe5e9050db2f8f187288449656c64c9ad1bee3a4
Jose Vazquez <vazqjose@gmail.com>
2021-08-09 05:15:54 +0000

    t6

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -4,7 +4,7 @@ Write a python file that contains the class definition of a
 State and an instance Base = declarative_base():
 
     State class:
-    - inherits from Base Tips
+    - inherits from Base
     - links to the MySQL table states
     - class attribute id that represents a column of an
       auto-generated, unique integer, can’t be null and is a primary key
