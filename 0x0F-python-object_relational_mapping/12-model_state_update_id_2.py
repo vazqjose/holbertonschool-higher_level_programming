152692e230bc0f8e7cc4df910df40839b9c1520a
Jose Vazquez <vazqjose@gmail.com>
2021-08-13 02:22:51 +0000

    v1

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Write a script that changes the name of a State object from the database
+    - Your script should take 3 arguments: mysql username, mysql
+        password and database name
+    - You must use the module SQLAlchemy
+    - You must import State and Base from model_state - from
+        model_state import Base, State
+    - Print the new states.id after creation
+    - Your code should not be executed when imported
+"""
+
+
+import sys
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+
+if __name__ == "__main__":
+    user = sys.argv[1]
+    passwd = sys.argv[2]
+    dbname = sys.argv[3]
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        user, passwd, dbname), pool_pre_ping=True)
+
+    Session = sessionmaker(engine)
+    mySession = Session()
+
+    mySession.query(State).filter(State.id == 2).\
+        update({"name": "New Mexico"}, synchronize_session="fetch")
+    mySession.commit()
