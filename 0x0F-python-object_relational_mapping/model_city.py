54df73bd82817344f21ba873d4cc93b05466ffa1
Jose Vazquez <vazqjose@gmail.com>
2021-08-13 05:05:31 +0000

    typo fix

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python3
-''' Define class City that inherits from Base '''
+''' Defines class City that inherits from
+Base '''
 
 
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, Foreignkey
+from sqlalchemy import Column, Integer, String, ForeignKey
 from model_state import State, Base
 
 Base = declarative_base()
 
 
-class City(State):
-    ''' Defines a State from table cities '''
+class City(Base):
+    ''' Defines a City '''
 
     __tablename__ = 'cities'
 
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey(State.id), nullable=False))
+    state_id = Column(Integer, ForeignKey(State.id), nullable=False)
