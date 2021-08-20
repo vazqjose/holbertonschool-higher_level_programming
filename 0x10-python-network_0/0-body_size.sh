f481601f76ae5673ff6332799acb4bbe4e23d9a6
Jose Vazquez <vazqjose@gmail.com>
2021-08-20 03:01:38 +0000

    t0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script that takes in a URL, returns size of body
-curl -s $1 | grep -i Content-Length | awk '{print $2}'
+curl -s $1 --include | grep -i Content-Length | awk '{print $2}'
