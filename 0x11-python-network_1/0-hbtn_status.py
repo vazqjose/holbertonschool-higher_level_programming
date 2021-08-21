69d0f41e2cd0a465ec27cddb5eaf24282243315a
Jose Vazquez <vazqjose@gmail.com>
2021-08-21 02:13:43 +0000

    t0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+'''
+Write a Python script that fetches an https address
+    - You must use a with statement
+    - You must only use the package urllib
+    - You must use a with statement
+    - Output should be (tabulation before each - ):
+        Body response:$
+            - type: <class 'bytes'>
+            - content: b'OK'
+            - utf8 content: OK
+'''
+
+
+if __name__ == "__main__":
+
+    import urllib.request
+    with urllib.request.urlopen("https://intranet.hbtn.io/status") as response:
+        content = response.read()
+        print ("Body response:")
+        print ("\t- type: {}".format(type(content)))
+        print ("\t- content: {}".format(content))
+        print ("\t- utf8 content: {}".format(content.decode('utf-8')))
+
