5e24bce882cd6d05049a6d7bde6d5fd7ca401193
Jose Vazquez <vazqjose@gmail.com>
2021-08-24 20:12:33 +0000

    t1

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script that takes in a URL, sends GET request, returns response body
-if (( $(curl -s -o /dev/null -w "%{http_code}" $1) == 200)) ; then curl -s $1 ; fi
+curl -L -s $1
