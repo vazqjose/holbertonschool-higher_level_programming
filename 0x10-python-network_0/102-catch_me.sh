c2db72f6dd0d306bdfc92c37b661c8546437243d
Jose Vazquez <vazqjose@gmail.com>
2021-08-25 03:33:16 +0000

    bonus3

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script displays that causes the server to respond with a message
-curl -s -L --local-port 5000 "0.0.0.0:5000/catch_me" "$1"
+curl -s -L --local-port 5000 "0.0.0.0:5000/catch_me"
