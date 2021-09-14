bb3c3e62967bdcc27f6230c71c3a4f7455a5c513
Jose Vazquez <vazqjose@gmail.com>
2021-09-14 23:43:08 +0000

    t2

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const request = require.request;
+const url = process.argv[2];
+
+request(url, function (error, res, content) {
+  if (error) {
+    console.error(error);
+  }
+  console.log(res.statusCode);
+});
